2166f35b5696dccb9a041cc0415d5215b2ace168
edysetyobayuaji <edysetyobayuaji2@gmail.com>
2021-02-05 14:34:34 +0700

    perubahan title

--- resources/views/layouts/app_web.blade.php
+++ resources/views/layouts/app_web.blade.php
@@ -7,7 +7,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>PT Krakatau Steel (Persero), Tbk.</title>
+    <title>PT KPDP</title>
     <meta name="description" content="">
     <link rel="stylesheet" href="{{url('public/web/css/bootstrap-grid.css')}}">
     <link rel="stylesheet" href="{{url('public/web/css/style.css')}}">
